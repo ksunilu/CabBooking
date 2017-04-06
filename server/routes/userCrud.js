ac11c18ae133a1088dd6c5a0d93b3a8f88a16de5
Sunil Kumar <ksunilu@gmail.com>
2017-04-06 19:17:51 +0530

    .

--- server/routes/userCrud.js
+++ server/routes/userCrud.js
@@ -45,9 +45,6 @@ module.exports = function (app) {
 
     model.findOne({ email: req.body.email },
       function (err, usrData) {
-        console.log(err);
-        console.log(usrData);
-
         if (err) {
           res.json(err);
           console.log('Error at /login : ' + err);
