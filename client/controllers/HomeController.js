af6bd3ee57554a9629deb7c323355b057582cbb7
Sunil Kumar <ksunilu@gmail.com>
2017-04-24 14:16:32 +0530

    .

--- client/controllers/HomeController.js
+++ client/controllers/HomeController.js
@@ -8,7 +8,7 @@ angular.module('myApp')
                 center: { lat: 28.61, lng: 77.23 },
                 zoom: 10
             });
-            // $scope.initMap();
+
 
             var input = document.getElementById('txtFrom');
             // map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
@@ -23,15 +23,12 @@ angular.module('myApp')
             });
 
             autocomplete.addListener('place_changed', function () {
-                infowindow.close();
                 marker.setVisible(false);
                 var place = autocomplete.getPlace();
                 if (!place.geometry) {
                     window.alert("Autocomplete's returned place contains no geometry");
                     return;
                 }
-
-                // If the place has a geometry, then present it on a map.
                 if (place.geometry.viewport) {
                     map.fitBounds(place.geometry.viewport);
                 } else {
@@ -49,32 +46,6 @@ angular.module('myApp')
 
                 marker.setPosition(place.geometry.location);
                 marker.setVisible(true);
-
-                // var address = '';
-                // if (place.address_components) {
-                //     address = [
-                //         (place.address_components[0] && place.address_components[0].short_name || ''),
-                //         (place.address_components[1] && place.address_components[1].short_name || ''),
-                //         (place.address_components[2] && place.address_components[2].short_name || '')
-                //     ].join(' ');
-                // }
-
-                // infowindow.setContent('<div><strong>' + place.name + '</strong><br>' + address);
-                // infowindow.open(map, marker);
-
-                //Location details
-                // for (var i = 0; i < place.address_components.length; i++) {
-                //     if (place.address_components[i].types[0] == 'postal_code') {
-                //         document.getElementById('postal_code').innerHTML = place.address_components[i].long_name;
-                //     }
-                //     if (place.address_components[i].types[0] == 'country') {
-                //         document.getElementById('country').innerHTML = place.address_components[i].long_name;
-                //     }
-                // }
-                // document.getElementById('location').innerHTML = place.formatted_address;
-                // document.getElementById('lat').innerHTML = place.geometry.location.lat();
-                // document.getElementById('lon').innerHTML = place.geometry.location.lng();
-                // document.getElementById('all').innerHTML = JSON.stringify(place);
             });
         }
 
