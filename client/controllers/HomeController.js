42e62f730eced6bb8a15e405bd1650f56521ca8b
Sunil Kumar <ksunilu@gmail.com>
2017-05-11 18:07:03 +0530

    .

--- client/controllers/HomeController.js
+++ client/controllers/HomeController.js
@@ -1,8 +1,8 @@
 angular.module('myApp')
     .controller('HomeController', function ($scope, $http, crudService) {
 
-        $scope.book = {};
-        var source, destination;
+
+        // var source, destination;
         var directionsDisplay;
         var directionsService = new google.maps.DirectionsService();
 
@@ -10,7 +10,7 @@ angular.module('myApp')
         function initData() {
             console.log('Trying get all data.');
             $scope.alltariff = {};
-            $scope.Data = {};
+            $scope.rec = {};
 
             var promise = crudService.getAllData('/tariffs');
             promise.then(function (data) {
@@ -22,41 +22,50 @@ angular.module('myApp')
         ///////////////////////// // map code starts ////////////////////////////
 
         $scope.initMap = function () {
+
+            var map = new google.maps.Map(document.getElementById('map'), {
+                center: { lat: 28.61, lng: 77.23 },
+                zoom: 10
+            });
+
             new google.maps.places.SearchBox(document.getElementById('txtFrom'));
             new google.maps.places.SearchBox(document.getElementById('txtTo'));
             directionsDisplay = new google.maps.DirectionsRenderer({ 'draggable': true });
+            directionsDisplay.setMap(map);
         }
 
         google.maps.event.addDomListener(document.getElementById('routeClick'), 'click',
             function () {
 
-
                 //*********DIRECTIONS AND ROUTE**********************//
-                source = document.getElementById('txtFrom').value;
-                destination = document.getElementById('txtTo').value;
+                $scope.rec.bookSource = document.getElementById('txtFrom').value;
+                $scope.rec.bookDestination = document.getElementById('txtTo').value;
 
                 var request = {
-                    origin: source,
-                    destination: destination,
+                    origin: $scope.rec.bookSource,
+                    destination: $scope.rec.bookDestination,
                     travelMode: google.maps.TravelMode.DRIVING
                 };
                 directionsService.route(request, function (response, status) {
                     if (status == google.maps.DirectionsStatus.OK) {
                         directionsDisplay.setDirections(response);
                     }
+                    else
+                        throw 'unable to route';
                 });
 
                 //*********DISTANCE AND DURATION**********************//
                 var service = new google.maps.DistanceMatrixService();
                 service.getDistanceMatrix({
-                    origins: [source],
-                    destinations: [destination],
+                    origins: [$scope.rec.bookSource],
+                    destinations: [$scope.rec.bookDestination],
                     travelMode: google.maps.TravelMode.DRIVING,
                     unitSystem: google.maps.UnitSystem.METRIC,
                     avoidHighways: false,
                     avoidTolls: false
                 }, function (response, status) {
-                    if (status == google.maps.DistanceMatrixStatus.OK && response.rows[0].elements[0].status != 'ZERO_RESULTS') {
+                    if (status == google.maps.DistanceMatrixStatus.OK &&
+                        response.rows[0].elements[0].status != 'ZERO_RESULTS') {
                         var distance = response.rows[0].elements[0].distance.text;
                         var duration = response.rows[0].elements[0].duration.text;
                         var dvDistance = document.getElementById('dvDistance');
