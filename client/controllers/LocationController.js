51e6a9906b1e6cb9319d248d12763fe48b325b85
Sunil Kumar <ksunilu@gmail.com>
2017-07-07 12:50:49 +0530

    location completed with icon

--- client/controllers/LocationController.js
+++ client/controllers/LocationController.js
@@ -6,6 +6,7 @@ angular.module('myApp')
                 loc = { lat: position.coords.latitude, lng: position.coords.longitude };
                 drawInitMap(loc);
                 $scope.location = loc;
+                //broad cast location
             });
             return;
             //code end
@@ -19,34 +20,30 @@ angular.module('myApp')
                 });
                 marker = new google.maps.Marker({
                     map: map,
-                    position: location
+                    position: location,
+                    icon: '../public/images/cab.png',
+                    size: new google.maps.Size(36, 36)
                 });
                 infoWindow = new google.maps.InfoWindow({
-                    content: 'Your Location.' + JSON.stringify(location)
+                    content: 'Your Location.'
                 });
                 infoWindow.open(map, marker);
                 map.setCenter(location);
-                initTextbox(map, location);
+                initTextbox(map, location, marker, infoWindow);
             }
-            function initTextbox(map, location) {
-                var directionsDisplay;
-                var directionsService = new google.maps.DirectionsService();
+            function initTextbox(map, location, marker, infoWindow) {
+                // var directionsDisplay;
+                // var directionsService = new google.maps.DirectionsService();
                 var inputFrom = document.getElementById('txtFrom');
                 map.controls[google.maps.ControlPosition.TOP_LEFT].push(inputFrom);
-
                 var autocomplete = new google.maps.places.Autocomplete(inputFrom);
                 autocomplete.bindTo('bounds', map);
 
-                var infowindow = new google.maps.InfoWindow();
-
-                var marker = new google.maps.Marker({
-                    map: map,
-                    position: location
-
-                });
-
                 autocomplete.addListener('place_changed', function () {
+
                     marker.setVisible(false);
+                    infoWindow.close();
+
                     var place = autocomplete.getPlace();
                     if (!place.geometry) {
                         window.alert("Autocomplete's returned place contains no geometry");
@@ -59,16 +56,10 @@ angular.module('myApp')
                         map.setZoom(10);
                     }
 
-                    marker.setIcon(({
-                        url: place.icon,
-                        size: new google.maps.Size(71, 71),
-                        origin: new google.maps.Point(0, 0),
-                        anchor: new google.maps.Point(17, 34),
-                        scaledSize: new google.maps.Size(35, 35)
-                    }));
-
                     marker.setPosition(place.geometry.location);
                     marker.setVisible(true);
+                    infoWindow.setContent('Your typed Location.');
+                    infoWindow.open(map, marker);
                     $scope.location = place.geometry.location;
                 });
             }
