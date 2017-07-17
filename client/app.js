bfa590822ca2de5a8d3f95725d468f29e8f548fb
Sunil Kumar <ksunilu@gmail.com>
2017-07-17 13:10:17 +0530

    unauthorised page access comp.

--- client/app.js
+++ client/app.js
@@ -45,9 +45,9 @@ app.run(function ($rootScope, $http, $location, $sessionStorage, $cookies, Authe
 
     // redirect to login page if not logged in and trying to access a restricted page
     $rootScope.$on('$locationChangeStart', function (event, next, current) {
-        $rootScope.currentUser = AuthenticationService.GetUser();
+        var user = $rootScope.currentUser = AuthenticationService.GetUser();
 
-        var publicPages = ['/', '/login', '/register', '/error'];
+        var publicPages = ['/', '/login', '/register'];
         var webPages = ['/', '/book', '/tariffs', '/login', '/register', '/register-driver', '/location', '/rides', '/password'];
 
         var adminPages = ['/', '/login', '/password', '/tariffs', '/register-driver'];
@@ -58,23 +58,36 @@ app.run(function ($rootScope, $http, $location, $sessionStorage, $cookies, Authe
 
         var restrictedPage = publicPages.indexOf($location.path()) === -1;
         var pageNotFound = webPages.indexOf($location.path()) === -1;
-        var notDriverPage = driverPages.indexOf($location.path()) === -1;
-        var notAdminPage = adminPages.indexOf($location.path()) === -1;
-        var notClientPage = clientPages.indexOf($location.path()) === -1;
+
+        var notDriverPage = driverPages.indexOf($location.path()) === -1 && $location.path() != '';
+        var notAdminPage = adminPages.indexOf($location.path()) === -1 && $location.path() != '';
+        var notClientPage = clientPages.indexOf($location.path()) === -1 && $location.path() != '';
 
         // var authUser = $cookies.getObject('authUser');
         // if (authUser != undefined) {
         //     var loggedInUser = authUser.currentUser.userInfo;
         // }
-        if (pageNotFound && $location.path() != '') {
-            $location.path('/error');
-        }
-        else if (restrictedPage && !$sessionStorage.tokenDetails && $location.path() != '') {
+
+
+        if (restrictedPage && !$sessionStorage.tokenDetails && $location.path() != '') {
             $location.path('/login');
         }
-
-
+        else if ($sessionStorage.tokenDetails) {
+            if (pageNotFound && $location.path() != '') {
+                //set global error
+                alert('Error: Page not found')
+                $location.path('/');
+            }
+            else if (user.role === 'admin' && notAdminPage ||
+                user.role === 'driver' && notDriverPage ||
+                user.role === 'client' && notClientPage) {
+                //ill leagel page access
+                alert('Error: Illegal page request!')
+                $location.path('/');
+            }
+        }
         // console.log(restrictedPage);
         // console.log($sessionStorage.tokenDetails);
+
     });
 });
